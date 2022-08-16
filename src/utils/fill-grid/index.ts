57e9a81730edfd3fd30e1ed491106a0d99eb1681
Junyan Ren <ren.ju@northeastern.edu>
2022-08-16 18:08:16 -0400

    update sudoku grid logic

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -1,19 +1,56 @@
-import { GRID } from "../../typings"
+import { shuffle, isInRow, isInCol } from 'utils'
+import { GRID, NUMBERS } from '../../typings'
 
 const gridExample: GRID = [
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
-function fillGrid(grid:GRID){
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+/**
+ * A backtracking/recursive function to check all the possible combinations of numbers until a solution is found
+ * @param grid 9X9 Sudoku grid
+ */
+function fillGrid(grid: GRID) {
+  let row = 0
+  let col = 0
+
+  for (let i = 0; i < 81; i++) {
+    row = Math.floor(i / 9)
+    col = i % 9
+
+    if (grid[row][col] === 0) {
+      shuffle(numbers)
+
+      for (let value of numbers) {
+        //check whether it is duplicated
+        if (!isInRow({ grid, row, value })) {
+          if (!isInCol({ col, grid, value })) {
+            const square = [
+              [0,0,0],
+              [0,0,0],
+              [0,0,0]
+            ]
+            grid[row][col] = value
+            //check grid if it is full, if yes, stop and return
+            //otherwise we run fillGrid(grid)
+          }
+        }
+      }
+
+      break
+    }
+  }
+
+  grid[row][col] = 0
 }
 
 export default fillGrid
